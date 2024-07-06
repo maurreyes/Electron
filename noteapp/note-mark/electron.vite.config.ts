d85153983f446306bce4d91c4d6b83a2782283fb
maurreyes <rezamar555@gmail.com>
2024-07-05 18:57:55 -0600

    Modifying electron main paths

--- noteapp/note-mark/electron.vite.config.ts
+++ noteapp/note-mark/electron.vite.config.ts
@@ -7,14 +7,30 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   preload: {
+    plugins: [externalizeDepsPlugin()],
+    resolve: {
+      alias: {
+        '@/lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared')
+      }
+    }
+  },
+  preload: {
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    assetsInclude: 'src/renderer/assets/**',
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'),
+        '@/hooks': resolve('src/renderer/src/hooks'),
+        '@/assets': resolve('src/renderer/src/assets'),
+        '@/store': resolve('src/renderer/src/store'),
+        '@/components': resolve('src/renderer/src/components'),
+        '@/mocks': resolve('src/renderer/src/mocks'),
       }
     },
     plugins: [react()]
   }
-})
+}
